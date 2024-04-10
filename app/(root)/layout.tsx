15d586bb189f0ae81fd21224bd00e94c63580059
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-10 14:11:03 -0400

    Modify the '(root)', 'meeting' layouts.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,7 @@
 import React, { ReactNode } from 'react';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-	return (
-		<main>
-			NavBar
-			{children}
-			Footer
-		</main>
-	);
+	return <main>{children}</main>;
 };
 
 export default RootLayout;
