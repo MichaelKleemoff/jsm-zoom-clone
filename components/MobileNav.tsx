9e5e5eba88ee7d8d82db3ae3c527b2f36463ebbb
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-11 17:15:33 -0400

    Reuse code for mapping over 'sidebarLinks'.

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,18 +1,22 @@
+'use client';
+
 import React from 'react';
 
 import {
 	Sheet,
 	SheetClose,
 	SheetContent,
-	SheetDescription,
-	SheetHeader,
-	SheetTitle,
 	SheetTrigger,
 } from '@/components/ui/sheet';
 import Image from 'next/image';
 import Link from 'next/link';
+import { sidebarLinks } from '@/constants';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 const MobileNav = () => {
+	const pathname = usePathname();
+
 	return (
 		<section className='w-full max-w-[264px]'>
 			<Sheet>
@@ -40,7 +44,34 @@ const MobileNav = () => {
 					{/* Remove the height of the NavBar while giving this div full height (100vh). 'overflow-y-auto' so we don't get a scroll bar. 'flex-col' so the links appear underneath each other. */}
 					<div className='flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto'>
 						<SheetClose asChild>
-							<section className='flex h-full flex-col gap-6 pt-16 text-white'></section>
+							<section className='flex h-full flex-col gap-6 pt-16 text-white'>
+								{sidebarLinks.map((link) => {
+									const isActive = pathname === link.route;
+
+									return (
+										<SheetClose asChild key={link.route}>
+											<Link
+												href={link.route}
+												key={link.label}
+												className={cn(
+													'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
+													{
+														'bg-blue-1': isActive,
+													}
+												)}
+											>
+												<Image
+													src={link.imgUrl}
+													alt={link.label}
+													width={20}
+													height={20}
+												/>
+												<p className='font-semibold'>{link.label}</p>
+											</Link>
+										</SheetClose>
+									);
+								})}
+							</section>
 						</SheetClose>
 					</div>
 				</SheetContent>
