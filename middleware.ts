298902a29d06a62ad06075df04bc28f64986e409
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-11 18:39:17 -0400

    Import 'createRouteMatcher' into 'middleware.ts'.

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware, createRouteMatcher } from '@clerk/nextjs';
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
