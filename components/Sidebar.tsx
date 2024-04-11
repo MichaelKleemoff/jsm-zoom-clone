0ea4a3a84571a685efab5cfc3eee310bee4dac10
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-11 12:17:39 -0400

    Add a 'p' tag for each link's label and style it.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -35,6 +35,9 @@ const Sidebar = () => {
 								width={24}
 								height={24}
 							/>
+							<p className='text-lg font-semibold max-lg:hidden'>
+								{link.label}
+							</p>
 						</Link>
 					);
 				})}
