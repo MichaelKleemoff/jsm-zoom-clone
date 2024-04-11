fe50a4f65a6b7f253cb7f52b34e69efdf46a0d05
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-11 16:33:07 -0400

    Import 'SheetClose' as child and add a section with basic styling.

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {
 	Sheet,
+	SheetClose,
 	SheetContent,
 	SheetDescription,
 	SheetHeader,
@@ -35,6 +36,13 @@ const MobileNav = () => {
 						/>
 						<p className='text-[26px] font-extrabold text-white'>Yoom</p>
 					</Link>
+
+					{/* Remove the height of the NavBar while giving this div full height (100vh). 'overflow-y-auto' so we don't get a scroll bar. 'flex-col' so the links appear underneath each other. */}
+					<div className='flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto'>
+						<SheetClose asChild>
+							<section className='flex h-full flex-col gap-6 pt-16 text-white'></section>
+						</SheetClose>
+					</div>
 				</SheetContent>
 			</Sheet>
 		</section>
