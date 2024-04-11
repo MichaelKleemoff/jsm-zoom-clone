f0e74175721d0bee90945172bb51e49467b72189
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-11 09:26:04 -0400

    Begin building 'Sidebar'.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	return (
+		<section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+			Sidebar
+		</section>
+	);
 };
 
 export default Sidebar;
