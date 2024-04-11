4d91f1624b24fcb7591f674d257e2b23bc430dbf
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-11 15:21:53 -0400

    Add logo and basic layout to 'NavBar'.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react';
 
 const NavBar = () => {
-	return <div>NavBar</div>;
+	return (
+		<nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+			<Link href='/' className='flex items-center gap-1'>
+				<Image src='/icons/logo.svg' width={32} height={32} alt='Yoom' />
+			</Link>
+		</nav>
+	);
 };
 
 export default NavBar;
