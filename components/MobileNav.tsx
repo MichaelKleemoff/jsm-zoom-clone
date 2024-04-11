493992aa601b274ae23de568b4696aa98c1cfed8
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-11 15:56:14 -0400

    Change 'SheetTrigger' to hamburger icon and make it a child of 'Sheet'.

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -8,12 +8,21 @@ import {
 	SheetTitle,
 	SheetTrigger,
 } from '@/components/ui/sheet';
+import Image from 'next/image';
 
 const MobileNav = () => {
 	return (
 		<section className='w-full max-w-[264px]'>
 			<Sheet>
-				<SheetTrigger>Open</SheetTrigger>
+				<SheetTrigger asChild>
+					<Image
+						src='/icons/hamburger.svg'
+						width={36}
+						height={36}
+						alt='hamburger icon'
+						className='cursor-pointer sm:hidden'
+					/>
+				</SheetTrigger>
 				<SheetContent>
 					<SheetHeader>
 						<SheetTitle>Are you absolutely sure?</SheetTitle>
