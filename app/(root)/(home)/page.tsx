479786ad18c5b761013a6cf7698b1bdd8d46d9b7
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-12 13:16:39 -0400

    Style 'h2' in banner.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,7 +6,9 @@ const Home = () => {
 			{/* Banner */}
 			<div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
 				<div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-					<h2>Upcoming Meeting: 12:30 PM</h2>
+					<h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
+						Upcoming Meeting: 12:30 PM
+					</h2>
 				</div>
 			</div>
 		</section>
