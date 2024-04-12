0fb549d244c983cd83dc4b3590924996713f02d1
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-12 18:15:04 -0400

    Style and render all cards in 'MeetingTypeList' dynamically.

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -1,27 +1,39 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
-
 import React from 'react';
 
-const HomeCard = () => {
+interface HomeCardProps {
+	className: string;
+	img: string;
+	title: string;
+	description: string;
+	handleClick: () => void;
+}
+
+const HomeCard = ({
+	className,
+	img,
+	title,
+	description,
+	handleClick,
+}: HomeCardProps) => {
 	return (
 		<div
-			className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer'
-			onClick={() => {}}
+			className={cn(
+				'px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',
+				className
+			)}
+			onClick={handleClick}
 		>
 			<div className='flex-center glassmorphism size-12 rounded-[10px]'>
-				<Image
-					src='/icons/add-meeting.svg'
-					alt='meeting'
-					width={27}
-					height={27}
-				/>
+				<Image src={img} alt={title} width={27} height={27} />
 			</div>
 
 			<div className='flex flex-col gap-2'>
-				<h1 className='text-2xl font-bold'>New Meeting</h1>
-				<p className='text-lg font-normal'>Start an instant meeting</p>
+				<h1 className='text-2xl font-bold'>{title}</h1>
+				<p className='text-lg font-normal'>{description}</p>
 			</div>
 		</div>
 	);
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,12 +1,45 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import HomeCard from './HomeCard';
+import { useRouter } from 'next/navigation';
 
 const MeetingTypeList = () => {
+	const router = useRouter();
+	const [meetingState, setMeetingState] = useState<
+		'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
+	>();
+
 	return (
 		<section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-			<HomeCard />
+			<HomeCard
+				img='/icons/add-meeting.svg'
+				title='New Meeting'
+				description='Start an instant meeting'
+				handleClick={() => setMeetingState('isInstantMeeting')}
+				className='bg-orange-1'
+			/>
+			<HomeCard
+				img='/icons/join-meeting.svg'
+				title='Join Meeting'
+				description='Via invitation link'
+				handleClick={() => setMeetingState('isJoiningMeeting')}
+				className='bg-blue-1'
+			/>
+			<HomeCard
+				img='/icons/schedule.svg'
+				title='Schedule Meeting'
+				description='Plan your meeting'
+				handleClick={() => setMeetingState('isScheduleMeeting')}
+				className='bg-purple-1'
+			/>
+			<HomeCard
+				img='/icons/recordings.svg'
+				title='View Recordings'
+				description='Meeting recordings'
+				handleClick={() => router.push('/recordings')}
+				className='bg-yellow-1'
+			/>
 		</section>
 	);
 };
