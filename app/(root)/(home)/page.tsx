6b9ddb928553dc018578150fefc320873f126b79
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-12 12:58:33 -0400

    Add 'h2' subheading to hero.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,11 @@ const Home = () => {
 	return (
 		<section className='flex size-full flex-col gap-10 text-white'>
 			{/* Banner */}
-			<div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'></div>
+			<div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+				<div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+					<h2>Upcoming Meeting: 12:30 PM</h2>
+				</div>
+			</div>
 		</section>
 	);
 };
