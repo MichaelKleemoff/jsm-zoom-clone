e835a611a1bb90f51f037d72fa5bd08152ac9db9
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-12 10:03:46 -0400

    Create 'protectedRoutes' invoking 'createRouteMatcher' on existing private routes.

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+const protectedRoutes = createRouteMatcher([
+	'/',
+	'/upcoming',
+	'/previous',
+	'/recordings',
+	'/personal-room',
+	'/meeting(.*)',
+]);
+
 export default clerkMiddleware();
 
 export const config = {
