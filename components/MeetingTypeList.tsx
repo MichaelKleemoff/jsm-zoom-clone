e07e5f175503ec2ccb395665f44d093e931425c1
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 12:46:12 -0400

    Create 'createMeeting' function in 'MeetingTypeList'.

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -11,6 +11,8 @@ const MeetingTypeList = () => {
 		'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
 	>();
 
+	const createMeeting = () => {};
+
 	return (
 		<section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
 			<HomeCard
