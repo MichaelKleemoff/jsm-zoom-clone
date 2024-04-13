b1e0c613054b7c07c7dfc1016eaab0652678bb75
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 16:11:44 -0400

    Import/implement 'Image', 'image'.

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -9,6 +9,9 @@ import {
 	DialogTrigger,
 } from '@/components/ui/dialog';
 
+import Image from 'next/image';
+import { Divide } from 'lucide-react';
+
 interface MeetingModalProps {
 	isOpen: boolean;
 	onClose: () => void;
@@ -34,7 +37,15 @@ const MeetingModal = ({
 }: MeetingModalProps) => {
 	return (
 		<Dialog open={isOpen} onOpenChange={onClose}>
-			<DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'></DialogContent>
+			<DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'>
+				<div className='flex flex-col gap-6'>
+					{image && (
+						<div className='flex justify-center'>
+							<Image src={image} alt='image' width={72} height={72} />
+						</div>
+					)}
+				</div>
+			</DialogContent>
 		</Dialog>
 	);
 };
