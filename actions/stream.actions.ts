6165218c9c8601b3d71074b70f8a76bfd440e019
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 19:23:57 -0400

    Create 'tokenProvider' function and add a few guard clauses.

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -2,3 +2,11 @@
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_SECRET_KEY;
+
+export const tokenProvider = async () => {
+	const user = await currentUser();
+
+	if (!user) throw new Error('User is not logged in');
+	if (!apiKey) throw new Error('No API key');
+	if (!apiSecret) throw new Error('No API secret');
+};
