ca0faa1c90ba7a9eda44f9c2f5d6ce39f49e0c4b
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 18:03:22 -0400

    Return 'StreamVideoProvider' children.

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,8 +1,9 @@
 import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
+import { ReactNode } from 'react';
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-const StreamVideoProvider = () => {
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
 	return <StreamVideo client={client}></StreamVideo>;
 };
 
