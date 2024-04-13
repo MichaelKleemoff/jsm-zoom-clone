018d139ae73dc55f8b3422d02fc24670e883c009
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 18:06:08 -0400

    Create 'videoClient' hook.

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,9 +1,11 @@
 import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+	const [videoClient, setVideoClient] = useState();
+
 	return <StreamVideo client={client}></StreamVideo>;
 };
 
