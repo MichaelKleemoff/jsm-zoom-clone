b7f4aec51b3cde25956d384b3685830980c695aa
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 16:35:10 -0400

    Add 'Button' styling it, adding 'onClick' functionality and conditionally rendering a button icon.

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -10,8 +10,8 @@ import {
 } from '@/components/ui/dialog';
 
 import Image from 'next/image';
-import { Divide } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
 
 interface MeetingModalProps {
 	isOpen: boolean;
@@ -48,6 +48,22 @@ const MeetingModal = ({
 					<h1 className={cn('text-3xl font-bold leading-[42px]', className)}>
 						{title}
 					</h1>
+					{children}
+					<Button
+						className='bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0'
+						onClick={handleClick}
+					>
+						{buttonIcon && (
+							<Image
+								src={buttonIcon}
+								alt='button icon'
+								width={13}
+								height={13}
+							/>
+						)}{' '}
+						&nbsp;
+						{buttonText || 'Schedule Meeting'}
+					</Button>
 				</div>
 			</DialogContent>
 		</Dialog>
