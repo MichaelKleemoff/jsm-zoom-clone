b1ae466fc9eda28758aa5c426338dd8b58fabeb8
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 18:17:32 -0400

    Get 'user' from '@clerk/nextjs'.

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,3 +1,4 @@
+import { useUser } from '@clerk/nextjs';
 import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
 import { ReactNode, useEffect, useState } from 'react';
 
@@ -5,6 +6,9 @@ const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
 	const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+
+	// Get `user` from Clerk
+	const { user, isLoaded } = useUser();
 
 	useEffect(() => {}, []);
 
