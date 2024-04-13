1d2093d33d303068f69a30439f073004507d9794
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 16:02:32 -0400

    Style 'DialogContent'.

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -34,7 +34,7 @@ const MeetingModal = ({
 }: MeetingModalProps) => {
 	return (
 		<Dialog open={isOpen} onOpenChange={onClose}>
-			<DialogContent></DialogContent>
+			<DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'></DialogContent>
 		</Dialog>
 	);
 };
