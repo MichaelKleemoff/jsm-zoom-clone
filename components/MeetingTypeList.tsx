ca58064f64601b3accdbd7bc091bcb3a93df0741
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 10:05:52 -0400

    Create 'MeetingModal' component.

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,8 @@ const MeetingTypeList = () => {
 		'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
 	>();
 
+	console.log(meetingState);
+
 	return (
 		<section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
 			<HomeCard
