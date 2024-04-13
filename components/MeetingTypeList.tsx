a02bfd43b397eddc583966e05160bec0155114e4
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 10:10:56 -0400

    Import/implement 'MeetingModal' in 'MeetingTypeList'.

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,14 +3,13 @@
 import React, { useState } from 'react';
 import HomeCard from './HomeCard';
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
 	const router = useRouter();
 	const [meetingState, setMeetingState] = useState<
 		'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
 	>();
-
-	console.log(meetingState);
 
 	return (
 		<section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -42,6 +41,8 @@ const MeetingTypeList = () => {
 				handleClick={() => router.push('/recordings')}
 				className='bg-yellow-1'
 			/>
+
+			<MeetingModal />
 		</section>
 	);
 };
