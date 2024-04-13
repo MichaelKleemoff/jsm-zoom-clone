42b498389bd973801a63198100af75c85b0c5933
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 15:51:57 -0400

    Add 'open', 'onOpenChange' props to 'Dialog'. Pass in 'isOpen', 'onClose' as values.

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -32,7 +32,20 @@ const MeetingModal = ({
 	image,
 	buttonIcon,
 }: MeetingModalProps) => {
-	return <div>MeetingModal</div>;
+	return (
+		<Dialog open={isOpen} onOpenChange={onClose}>
+			<DialogTrigger>Open</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Are you absolutely sure?</DialogTitle>
+					<DialogDescription>
+						This action cannot be undone. This will permanently delete your
+						account and remove your data from our servers.
+					</DialogDescription>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	);
 };
 
 export default MeetingModal;
