fe21c841f45e07763fff99915e21197d3ad0ab3b
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 17:59:24 -0400

    Rename component to 'StreamVideoProvider'.

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -2,10 +2,8 @@ import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-export const App = () => {
-	return (
-		<StreamVideo client={client}>
-			<StreamCall call={call}></StreamCall>
-		</StreamVideo>
-	);
+const StreamVideoProvider = () => {
+	return <StreamVideo client={client}></StreamVideo>;
 };
+
+export default StreamVideoProvider;
