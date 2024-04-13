c4d7d7ed996a57d98999ea6bc4bdf642d99bdcd9
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-13 19:37:31 -0400

    Import 'currentUser' from '@clerk/nextjs/server'.

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -1,4 +1,7 @@
-'use server'; // Code here will only be run on the server.
+'use server';
+import { currentUser } from '@clerk/nextjs/server';
+
+// Code here will only be run on the server.
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_SECRET_KEY;
@@ -6,7 +9,10 @@ const apiSecret = process.env.STREAM_SECRET_KEY;
 export const tokenProvider = async () => {
 	const user = await currentUser();
 
+	// Guard clauses
 	if (!user) throw new Error('User is not logged in');
 	if (!apiKey) throw new Error('No API key');
 	if (!apiSecret) throw new Error('No API secret');
+
+	const streamClient = new streamClient();
 };
