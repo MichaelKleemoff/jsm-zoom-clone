69587740f43afff01bb0cceb31a695a49fa1072f
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-14 09:39:17 -0400

    Import/implement 'StreamCall', 'StreamTheme', 'useUser', 'useState'. Create, style 'main'. Create 'isSetupComplete' hook.

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react';
 
 const Meeting = ({ params }: { params: { id: string } }) => {
-	return <div>Meeting Room: #{params.id}</div>;
+	const { user, isLoaded } = useUser();
+	const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+	return (
+		<main className='h-screen w-full'>
+			<StreamCall>
+				<StreamTheme></StreamTheme>
+			</StreamCall>
+		</main>
+	);
 };
 
 export default Meeting;
