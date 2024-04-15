93adc59fb05c000211b1b7c7a18d633410cad69e
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-15 15:23:23 -0400

    Add a 'div' with 'input', 'label' to togglecamera/mic on/off.

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -26,6 +26,18 @@ const MeetingSetup = () => {
 		<div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
 			<h1 className='text-2xl font-bold'>Setup</h1>
 			<VideoPreview />
+			<div className='flex h-16 items-center justify-center gap-3'>
+				<label className='flex items-center justify-center gap-2 font-medium'>
+					<input
+						type='checkbox'
+						checked={isMicCamToggledOn}
+						onChange={(e) => {
+							setIsMicCamToggledOn(e.target.checked);
+						}}
+					/>
+					Join with mic and camera off
+				</label>
+			</div>
 		</div>
 	);
 };
