64acf7fbd0aafb526bc59d0244ad7b73bbed0fa8
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-15 12:35:10 -0400

    Create 'useEffect' with 'isMicCamToggledOn', 'call?.camera', 'call?.microphone' in the dependency array. Define 'call' with 'useCall()'.

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,8 +1,12 @@
-import { VideoPreview } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react';
+import { VideoPreview, useCall } from '@stream-io/video-react-sdk';
+import React, { useEffect, useState } from 'react';
 
 const MeetingSetup = () => {
 	const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+
+	const call = useCall();
+
+	useEffect(() => {}, [isMicCamToggledOn, call?.camera, call?.microphone]);
 
 	return (
 		<div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
