a9b9d4b53eac9bb61d2e455409f111e532bd5069
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-16 09:34:50 -0400

    Create a functional component 'CallLayout' to conditionally render layouts based on state using'stream-io' layout components.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,3 +1,4 @@
+import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react';
 
 // We can several layout styles. Define them as a `type` here.
@@ -5,6 +6,20 @@ type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
 	const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
+
+	// Create a new functional component which will render a specific layout depending on the current layout state.
+	const CallLayout = () => {
+		switch (layout) {
+			case 'grid':
+				return <PaginatedGridLayout />;
+			case 'speaker-right':
+				// If the speaker is on the right, the participants are placed on the left.
+				return <SpeakerLayout participantsBarPosition={'left'} />;
+			default:
+				// If the speaker is on the left, the participants are placed on the right.
+				return <SpeakerLayout participantsBarPosition={'right'} />;
+		}
+	};
 
 	return (
 		<section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
