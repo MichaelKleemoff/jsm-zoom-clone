04877ccc47458de536646c66f93fdd48381a8598
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-16 09:16:40 -0400

    Create 'layout' hook with initial state of 'speaker-left' in 'MeetingRoom'.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MeetingRoom = () => {
+	const [layout, setLayout] = useState('speaker-left');
+
 	return (
 		<section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
 			<div className='relative flex size-full items-center justify-center'>
