4924704831623ed474aef9fd7d2dcd442b673f58
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-16 09:10:45 -0400

    Nest a 'div' in 'section' and style it with basic layout in 'MeetingRoom'.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MeetingRoom = () => {
 	return (
 		<section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
-			MeetingRoom
+			<div className='relative flex size-full items-center justify-center'></div>
 		</section>
 	);
 };
