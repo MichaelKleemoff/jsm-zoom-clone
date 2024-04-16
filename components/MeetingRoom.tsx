b4c7333eacf43c57d3f8784829165e1d7b283696
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-16 09:13:15 -0400

    Nest another 'div' within previous 'div' and add some layout.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const MeetingRoom = () => {
 	return (
 		<section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
-			<div className='relative flex size-full items-center justify-center'></div>
+			<div className='relative flex size-full items-center justify-center'>
+				<div className='flex size-full max-w-[1000px] items-center'></div>
+			</div>
 		</section>
 	);
 };
