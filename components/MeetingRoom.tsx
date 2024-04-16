441c51908b8ccacfae1adbb8e15fd026067417a4
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-16 09:06:52 -0400

    Covert 'div' into 'section' with basic styling.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const MeetingRoom = () => {
-	return <div>MeetingRoom</div>;
+	return (
+		<section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+			MeetingRoom
+		</section>
+	);
 };
 
 export default MeetingRoom;
