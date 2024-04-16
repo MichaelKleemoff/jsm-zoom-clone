17b07613ea260df17878f3daf545f4ad9b704426
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-16 09:37:29 -0400

    Add 'CallLayout' to inner 'div'.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -24,7 +24,9 @@ const MeetingRoom = () => {
 	return (
 		<section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
 			<div className='relative flex size-full items-center justify-center'>
-				<div className='flex size-full max-w-[1000px] items-center'></div>
+				<div className='flex size-full max-w-[1000px] items-center'>
+					<CallLayout />
+				</div>
 			</div>
 		</section>
 	);
