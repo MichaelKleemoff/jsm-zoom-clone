61ed0c00ec06b1f9c9933d2b5aa1772212344de7
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-17 10:28:23 -0400

    Implement the 'dropdown-menu' starter components under 'CallControls' in 'MeetingRoom'.

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -60,6 +60,17 @@ const MeetingRoom = () => {
 				<CallControls />
 
 				{/* Dropdown menu */}
+				<DropdownMenu>
+					<DropdownMenuTrigger>Open</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>Profile</DropdownMenuItem>
+						<DropdownMenuItem>Billing</DropdownMenuItem>
+						<DropdownMenuItem>Team</DropdownMenuItem>
+						<DropdownMenuItem>Subscription</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			</div>
 		</section>
 	);
