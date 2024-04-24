608885a0cb363cf9661e25b80997e5894fb978fc
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-24 11:11:58 -0400

    Add 'use client' to 'EndCallButton.tsx' since a hook is used. Define 'call' using the 'useCall' hook.

--- components/ui/EndCallButton.tsx
+++ components/ui/EndCallButton.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import { useCall } from '@stream-io/video-react-sdk';
 import React from 'react';
 
 const EndCallButton = () => {
+	const call = useCall();
+
 	return <div>EndCallButton</div>;
 };
 
