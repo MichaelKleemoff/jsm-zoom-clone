362b2755fb676361fc8220f74fe93c6b55f6065c
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-24 11:20:49 -0400

    Define 'localParticipant' using the 'useLocalParticipant' from 'useCallStateHooks'.

--- components/ui/EndCallButton.tsx
+++ components/ui/EndCallButton.tsx
@@ -1,10 +1,13 @@
 'use client';
 
-import { useCall } from '@stream-io/video-react-sdk';
+import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk';
 import React from 'react';
 
 const EndCallButton = () => {
 	const call = useCall();
+
+	const { useLocalParticipant } = useCallStateHooks();
+	const localParticipant = useLocalParticipant();
 
 	return <div>EndCallButton</div>;
 };
