dd0c8fd7e8391caaa59fa88488a9305fb7caa0cc
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-25 12:16:39 -0400

    Add 'isLoading' hook.

--- hooks/useGetCalls.ts
+++ hooks/useGetCalls.ts
@@ -3,5 +3,6 @@ import { useState } from 'react';
 
 export const useGetCalls = () => {
 	const [calls, setCalls] = useState<Call[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
 	const client = useStreamVideoClient();
 };
