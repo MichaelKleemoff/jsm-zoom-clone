9cb18dd45fdca46b71cb778d1153b4ac43b791ba
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-25 12:22:45 -0400

    Define 'user' coming from 'clerk'. Add 'useEffect' with 'client', 'user?.id' in the dependency array.

--- hooks/useGetCalls.ts
+++ hooks/useGetCalls.ts
@@ -1,8 +1,13 @@
+import { useUser } from '@clerk/nextjs';
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useGetCalls = () => {
 	const [calls, setCalls] = useState<Call[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const client = useStreamVideoClient();
+	// Get calls for a specific user
+	const { user } = useUser();
+
+	useEffect(() => {}, [client, user?.id]);
 };
