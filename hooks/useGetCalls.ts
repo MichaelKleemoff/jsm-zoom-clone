6a67e2413dbf937beaa7db08085e98fb024f5fb9
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-25 12:14:59 -0400

    Define 'client' with 'useStreamVideoClient()' to be able to get the calls.

--- hooks/useGetCalls.ts
+++ hooks/useGetCalls.ts
@@ -1,6 +1,7 @@
-import { Call } from '@stream-io/video-react-sdk';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
 
 export const useGetCalls = () => {
 	const [calls, setCalls] = useState<Call[]>([]);
+	const client = useStreamVideoClient();
 };
