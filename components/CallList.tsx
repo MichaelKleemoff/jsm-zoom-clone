70b2772ade60b0295507a78839049792d80cb8bf
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-26 09:30:06 -0400

    Import 'useGetCalls' in 'CallList.tsx'. Destructure 4 returned items from 'useGetCalls'.

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,6 +1,10 @@
+import { useGetCalls } from '@/hooks/useGetCalls';
 import React from 'react';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
+	const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+		useGetCalls();
+
 	return <div>CallList</div>;
 };
 
