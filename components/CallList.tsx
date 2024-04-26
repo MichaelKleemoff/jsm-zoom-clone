77d6ce1d25f53fe3235ec82ce0f6c6c54797e9cb
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-26 09:48:41 -0400

    Create 'getCalls' function, 'recordings' hook.

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -2,11 +2,28 @@
 
 import { useGetCalls } from '@/hooks/useGetCalls';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 	const { endedCalls, upcomingCalls, callRecordings, isLoading } =
 		useGetCalls();
 	const router = useRouter();
+
+	const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+	// Will return the exact type of calls we want depending on the page we are on.
+	const getCalls = () => {
+		switch (type) {
+			case 'ended':
+				return endedCalls;
+			case 'upcoming':
+				return upcomingCalls;
+			case 'recordings':
+				return recordings;
+			default:
+				return [];
+		}
+	};
 
 	return <div>CallList</div>;
 };
