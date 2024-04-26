84024733401bbd1547e4f45ea85c745743927e08
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-26 09:33:53 -0400

    Add 'use client' to 'CallList.tsx'.

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,5 +1,6 @@
+'use client';
+
 import { useGetCalls } from '@/hooks/useGetCalls';
-import React from 'react';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 	const { endedCalls, upcomingCalls, callRecordings, isLoading } =
