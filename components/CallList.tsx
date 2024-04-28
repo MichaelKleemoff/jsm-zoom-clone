79ba4c58e844009be74bb659a7b07075b50cfd59
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-04-28 10:08:47 -0400

    Add an 'h1' rendering a 'noCallsMessage' if no calls exist to the 'ternary'.

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -43,10 +43,13 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
 	const noCallsMessage = getNoCallsMessage();
 
 	return <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>{
-		calls && calls.length > 0 ? calls.map((meeting:Call | CallRecording) => {
+		// If calls exist, `map` through `MeetingCard`(s), rendering them. Otherwise, render an `h1` with a `noCallsMessage`.
+		calls && calls.length > 0 ? calls.map((meeting:Call | CallRecording) => (
 			<MeetingCard />
-		})
-	}</div>;
+		) : (
+			<h1>{noCallsMessage}</h1>
+		))}
+	</div>
 };
 
 export default CallList;
